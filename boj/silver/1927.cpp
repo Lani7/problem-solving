fab4a97c26280cfe9fd6d52d9094d6fafba6e06b
Lani7 <appletree0272@gmail.com>
2024-04-08 18:16:08 +0900

    최소 힙

---
+++ boj/silver/1927.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <queue>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+using namespace std;
+
+struct cmp
+{
+  bool operator()(int o1, int o2)
+  {
+    return o1 > o2;
+  }
+};
+
+int main()
+{
+  fastio;
+
+  int n;
+  cin >> n;
+
+  priority_queue<int, vector<int>, cmp> pq;
+
+  for (int i = 0; i < n; i++)
+  {
+    int x;
+    cin >> x;
+
+    if (x != 0)
+      pq.push(x);
+    else if (!pq.empty())
+    {
+      cout << pq.top() << '\n';
+      pq.pop();
+    }
+    else
+      cout << "0\n";
+  }
+}
